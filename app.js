d6d3ba4b390ff8190fdd9363c4e1b75b8704d1fb
Aaron E J <120198986+Aaronjoshy@users.noreply.github.com>
2024-04-16 21:20:05 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,117 @@
+// Task1: initiate app and run server at 3000
+const express = require('express')
+const app=express(); 
+const postMod = require('./model/postmodel');
+
+app.use(express.json());
+
+
+
+const path=require('path');
+app.use(express.static(path.join(__dirname+'/dist/FrontEnd')));
+
+// Task2: create mongoDB connection 
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://aaronjoshy:12345@abcdef.cebxx2y.mongodb.net/emp?retryWrites=true&w=majority&appName=Abcdef")
+.then(()=>{
+    console.log("connected to DB")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+
+//Task 2 : write api with error handling and appropriate api mentioned in the TODO below
+
+app.use(express.json());
+
+
+
+//TODO: get data from db  using api '/api/employeelist'
+
+app.get('/api/employeelist',async(req,res)=>{
+    try{
+        const data =await postMod.find()
+        res.send(data);
+    }catch(error){
+        console.log(error)
+    }
+})
+
+
+
+
+//TODO: get single data from db  using api '/api/employeelist/:id'
+
+app.get('/api/employeelist/:id',async(req,res)=>{
+    try{
+        const id =req.params.id;
+       const data = await postMod.findById(id);
+        res.send(data);
+    }catch(error){
+        console.log(error)
+    }
+})
+
+
+
+//TODO: send data from db using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+//api
+app.post('/api/employeelist',async(req,res)=>{
+    try{
+        const data =req.body;
+        await postMod(data).save();
+        res.send({message:"Data added"})
+    }catch(error){
+        console.log(error)
+    }
+})
+
+
+
+
+//TODO: delete a employee data from db by using api '/api/employeelist/:id'
+
+
+app.delete('/api/employeelist/:id',async(req,res)=>{
+    try{
+        const id =req.params.id;
+        await postMod.findByIdAndDelete(id);
+        res.send({message:"Data deleted"});
+    }catch(error){
+        console.log(error)
+    }
+})
+
+
+
+
+//TODO: Update  a employee data from db by using api '/api/employeelist'
+//Request body format:{name:'',location:'',position:'',salary:''}
+
+app.put('/api/employeelist',async(req,res)=>{
+    const id= req.body._id
+    try{
+        
+        const final= await postMod.findByIdAndUpdate(id,req.body)
+        final.save();
+        res.send({message:"Data Updated"});
+    }catch(error){
+        console.log(error)
+    }
+});
+
+
+//! dont delete this code. it connects the front end file.
+app.listen(3000,()=>{
+    console.log('running in 3000');
+})
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
+});
+
+
+
